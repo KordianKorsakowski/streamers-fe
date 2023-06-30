279d5f88c9215c5fe550035ba0e3d903205dbdc7
Kordian Korsakowski <kordian.korsakowski@asterisk-dev.pl>
2023-06-30 09:19:16 +0200

    remove import

--- src/features/streamers/streamerForm/components/AddStreamerFormLogic.tsx
+++ src/features/streamers/streamerForm/components/AddStreamerFormLogic.tsx
@@ -1,13 +1,12 @@
 import { useFormikContext } from 'formik';
 import { useState } from 'react';
-import { Button } from '@mui/material';
 import { StreamerModal } from '../types/types';
 import { AddStreamerFormBody } from './AddStreamerFormBody';
 import { createStreamer } from '../../../../api/streamers/createStremer';
 import { useSnackbar } from '../../../../containers/SnackbarContainer';
 import { SubmitBtn } from '../../../../components/ui/components/SubmitBtn';
 import { useStreamers } from '../../../../containers/StreamersContainer';
-import { wait } from '../../../../utils/wait';
+
 export const AddStreamerFormLogic = () => {
   const { setSnackbar } = useSnackbar();
   const { setReloadList, setIsOpenForm } = useStreamers();
